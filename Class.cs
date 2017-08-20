5bb2b8377bfc44c594eb7d745541dbe220d2d1df
jiangguoliang <jiangguoliang@outlook.com>
2017-08-20 10:18:29 +0800

    Create Class.cs

---
+++ Class.cs
@@ -0,0 +1,22 @@
+#region Version Info
+
+/*
+*作者：UserName
+*日期：$time$
+*文件名：$safeitemname$
+*/
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+$if$ ($targetframeworkversion$ >= 4.5)using System.Threading.Tasks;
+$endif$
+namespace $rootnamespace$
+{
+    class $safeitemrootname$
+    {
+    }
+}
